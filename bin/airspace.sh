10ed143d2a42b9ba8394d2c2c4708fd7f438830e
Paul Baumgart <paul@proxv.com>
2013-04-30 21:01:19 -0700

    remove debug output

--- bin/airspace.sh
+++ bin/airspace.sh
@@ -1,7 +1,6 @@
 NW_VERSION=0.5.1
 
 cd "`cd $(dirname $0)/..; pwd`"
-pwd
 if [ "`uname`" == "Darwin" ]; then
   nw/node-webkit-v$NW_VERSION-osx-ia32/node-webkit.app/Contents/MacOS/node-webkit .
 elif [ "`uname`" == "Linux" ]; then
