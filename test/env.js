66a53c632d4001b07746b99c1d635ed89adf7a38
Paul Baumgart <paul@proxv.com>
2013-05-07 22:10:20 -0700

    remove unnecessary require

--- test/env.js
+++ test/env.js
@@ -1,4 +1,6 @@
-var Buffer = require('buffer');
+// This file creates the minimum requirements for
+// tests that depend on Backbone but that don't
+// require a DOM
 
 var window = global.window = {};
 window._ = global._ = require('underscore');
