aea0f1f37b8a554e939a95671d6d3278c522469a
Paul Baumgart <paul@proxv.com>
2013-05-08 20:22:56 -0700

    stub icon conversion in local-leap-app

--- app/models/local-leap-app.js
+++ app/models/local-leap-app.js
@@ -1,6 +1,16 @@
+var os = require('os');
 var LeapApp = require('./leap-app.js');
+var icns = require('../utils/icns.js');
 
 module.exports = LeapApp.extend({
+
+  constructor: function(args) {
+    if (args.rawIconFile) {
+      this.convertIcon(args.rawIconFile);
+      delete args.rawIconFile;
+    }
+    LeapApp.call(this, args);
+  },
 
   isLocalApp: function() {
     return true;
@@ -8,6 +18,10 @@ module.exports = LeapApp.extend({
 
   isStoreApp: function() {
     return false;
+  },
+
+  convertIcon: function(filename) {
+
   }
 
 });
