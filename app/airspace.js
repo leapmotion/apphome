4de3f20376c8c54a0caaa0d09a505e52efe183ee
Paul Baumgart <paul@proxv.com>
2013-05-15 20:33:08 -0700

    start organizing app controller

--- app/airspace.js
+++ app/airspace.js
@@ -4,10 +4,36 @@ var LeapAppCollection = require('./models/leap-app-collection.js');
 var LeapApp = require('./models/leap-app.js');
 var BuiltinTileApp = require('./models/builtin-tile-app.js');
 
+function AppController() {
+}
+
+AppController.prototype = {
+
+  runApp: function() {
+    this._setupGlobals();
+    this._paintPage();
+    this._initializeState();
+  },
+
+  _setupGlobals: function() {
+    global.uiGlobals = uiGlobals;
+    uiGlobals.leapApps = new LeapAppCollection();
+  },
+
+  _paintPage: function() {
+    $('body').append((new PageContainerView()).$el);
+  },
+
+  _initializeState: function() {
+    BuiltinTileApp.createBuiltinTiles();
+    LeapApp.hydrateCachedModels();
+  }
+
+};
+
+var appController;
+
 $(window.document).ready(function() {
-  global.uiGlobals = uiGlobals;
-  uiGlobals.leapApps = new LeapAppCollection();
-  $('body').append((new PageContainerView()).$el);
-  BuiltinTileApp.createBuiltinTiles();
-  LeapApp.hydrateCachedModels();
+  appController = new AppController();
+  appController.runApp();
 });
