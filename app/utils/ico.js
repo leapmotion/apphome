a6190abfb3ee7c8b5576b63465fd1f5d24981bf3
Paul Baumgart <paul@proxv.com>
2013-05-20 15:40:41 -0700

    just take the largest icon on Windows

--- app/utils/ico.js
+++ app/utils/ico.js
@@ -5,14 +5,12 @@ var path = require('path');
 var shell = require('./shell.js');
 
 function convertToPng(inputBinary, outputPng, cb) {
-  var size = 96;
-
   if (os.platform() !== 'win32') {
     return cb(new Error('ico conversion is only supported on Windows'));
   }
 
   exec(path.join(__dirname, '..', '..', 'bin', 'IconExtractor.exe') + ' ' +
-       shell.escape(inputBinary) + ' ' + shell.escape(outputPng) + ' ' + size, cb);
+       shell.escape(inputBinary) + ' ' + shell.escape(outputPng), cb);
 
 }
 
