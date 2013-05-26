83a3df2604540b05ef84d287bb4ba7ba5d5ae02b
Jeff Ferber <jeff@proxv.com>
2013-05-26 15:47:23 -0700

    fix leap-app-factory test helper

--- test/integration/suites/test-pagination.js
+++ test/integration/suites/test-pagination.js
@@ -1,7 +1,7 @@
 var integrationTest = require('../integration-test.js');
+var leapAppFactory = require('../../support/leap-app-factory.js');
 
 integrationTest.runInApp(__filename, function() {
-  var leapAppFactory = require('../../support/leap-app-factory.js');
 
   var leapApps;
   describe('pagination', function() {
