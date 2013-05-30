e361a3a9753e0821572a03651b0edbd91fbe2f60
Paul Baumgart <paul@proxv.com>
2013-05-29 20:06:53 -0700

    allow leap app factory to work outside of test mode

--- test/support/leap-app-factory.js
+++ test/support/leap-app-factory.js
@@ -1,43 +1,42 @@
-if (isRunningTest && isRunningTest()) {
-  var config = require('../../config/config.js');
-  var LeapApp = require('../../app/models/leap-app.js');
+var config = require('../../config/config.js');
+var LeapApp = require('../../app/models/leap-app.js');
 
-  var randomValue = require('./random-value.js');
+var randomValue = require('./random-value.js');
 
-  var randomInt = randomValue.randomInt;
-  var randomString = randomValue.randomString;
+var randomInt = randomValue.randomInt;
+var randomString = randomValue.randomString;
 
-  function storeAppData(args) {
-    args = args || {};
-    var verId = args.id || randomInt(5);
-    var appId = args.app_id || randomInt(3);
-    return {
-      id: verId,
-      appId: appId,
-      version: args.version || (randomInt(1) + '.' + randomInt(2) + '.' + randomInt(2)),
-      name: args.name || (randomString(7) + ' ' + randomString(5)),
-      binaryUrl: args.appurl || ('https://dev.leapmotion.com/apps/' + verId),
-      iconPath: config.Defaults.IconPath,
-      tilePath: config.Defaults.TilePath,
-      installedAt: (new Date()).getTime(),
-      state: LeapApp.States.Ready
-    };
-  }
-
-  function localAppData(args) {
-    args = args || {};
-    var id = randomString(16);
-
-    return {
-      id: args.id || id,
-      name: args.name || randomString(14),
-      iconPath: config.Defaults.IconPath,
-      tilePath: config.Defaults.TilePath,
-      installedAt: (new Date()).getTime(),
-      state: LeapApp.States.Ready
-    }
-  }
-
-  module.exports.storeAppData = storeAppData;
-  module.exports.localAppData = localAppData;
+function storeAppData(args) {
+  args = args || {};
+  var verId = args.id || randomInt(5);
+  var appId = args.app_id || randomInt(3);
+  return {
+    id: verId,
+    appId: appId,
+    version: args.version || (randomInt(1) + '.' + randomInt(2) + '.' + randomInt(2)),
+    name: args.name || (randomString(7) + ' ' + randomString(5)),
+    binaryUrl: args.appurl || ('https://dev.leapmotion.com/apps/' + verId),
+    iconPath: config.Defaults.IconPath,
+    tilePath: config.Defaults.TilePath,
+    installedAt: (new Date()).getTime(),
+    state: LeapApp.States.Ready
+  };
 }
+
+function localAppData(args) {
+  args = args || {};
+  var id = randomString(16);
+
+  return {
+    id: args.id || id,
+    name: args.name || randomString(14),
+    iconPath: config.Defaults.IconPath,
+    tilePath: config.Defaults.TilePath,
+    installedAt: (new Date()).getTime(),
+    state: LeapApp.States.Ready
+  }
+}
+
+module.exports.storeAppData = storeAppData;
+module.exports.localAppData = localAppData;
+
