d3c1b185e5b2acafd937833859f0180fec4d677c
Paul Baumgart <paul@proxv.com>
2013-05-31 16:31:49 -0700

    add drm module for writing user keys

---
+++ app/utils/drm.js
@@ -0,0 +1,34 @@
+var fs = require('fs-extra');
+var path = require('path');
+var os = require('os');
+
+var xmlTemplate = fs.readFileSync(path.join(__dirname, '..', '..', 'config', 'tt.xml.template'), 'utf-8');
+
+var PlatformOutputPaths = {
+  darwin: [ process.env.HOME, 'Library', 'Preferences', 'DSS', 'auth.plist' ],
+  win32: [ process.env.APPDATA, 'LeapMotion', 'tt.xml' ]
+};
+
+function generateXml(authId, token) {
+  return xmlTemplate.replace('@AUTHID', authId).replace('@TOKEN', token);
+}
+
+function writeXml(authId, token) {
+  var outputPath = path.join.apply(null, PlatformOutputPaths[os.platform()]);
+  if (outputPath) {
+    try {
+      var outputDir = path.dirname(outputPath);
+      if (!fs.existsSync(outputDir)) {
+        fs.mkdirsSync(outputDir);
+      }
+      fs.writeFileSync(outputPath, generateXml(authId, token));
+    } catch (err) {
+      console.error('Cannot write DRM XML for platform: ' + err.stack);
+    }
+  } else {
+    console.error('Cannot write DRM XML for platform: ' + os.platform());
+  }
+}
+
+writeXml(4000, 'abc123');
+
