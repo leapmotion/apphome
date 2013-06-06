3fde07ee2fbc1d72e0728ad18e7df387d4cac7c7
Max Carlson <max@maxcarlson.com>
2013-06-05 19:51:40 -0700

    Address Paul's comment

--- app/ui-globals.js
+++ app/ui-globals.js
@@ -5,9 +5,6 @@ var enumerable = require('./utils/enumerable.js');
 
 var LeapAppCollection = require('./models/leap-app-collection.js');
 
-// window.require is ugly but necessary according to https://github.com/rogerwang/node-webkit/issues/226
-var gui = window.require('nw.gui');
-
 var uiGlobals = _.extend({}, window.Backbone.Events);
 
 uiGlobals.Event = enumerable.make([
@@ -27,7 +24,7 @@ uiGlobals.scaling = 1;
 uiGlobals.appName = JSON.parse(fs.readFileSync(path.join(__dirname, '..', 'package.json'))).name;
 
 uiGlobals.sendNotification = function(header, body, icon) {
-  var win = gui.Window.get();
+  var win = nwGui.Window.get();
   window.LOCAL_NW.desktopNotifications.notify(icon || '', header, body, function() {
     win.focus();
     win.restore();
