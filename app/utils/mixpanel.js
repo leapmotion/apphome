a8eaa83d5a232f34a5be686ae2442a4ed2e76c26
Paul Baumgart <paul@proxv.com>
2013-06-26 14:28:24 -0700

    wrap mixpanel tracking in a domain

--- app/utils/mixpanel.js
+++ app/utils/mixpanel.js
@@ -1,15 +1,27 @@
+var domain = require('domain');
 var Mixpanel = require('mixpanel');
 
 var config = require('../../config/config.js');
 
-var mixpanel = Mixpanel.init(config.MixpanelToken);
+var mixpanelDomain = domain.create();
+mixpanelDomain.on('error', function(err) {
+  console.error('Error tracking Mixpanel event: ' + (err.stack || err));
+});
+
+var mixpanel;
+
+mixpanelDomain.run(function() {
+  mixpanel = Mixpanel.init(config.MixpanelToken);
+});
 
 function getTrackFn(eventName) {
   return function() {
     if (!/^(development|test)$/.test(process.env.LEAPHOME_ENV)) {
       console.log('Tracking Mixpanel event: ' + eventName);
-      mixpanel.track(uiGlobals.appName + ' - ' + eventName, {
-        version: uiGlobals.appVersion
+      mixpanelDomain.run(function() {
+        mixpanel.track(uiGlobals.appName + ' - ' + eventName, {
+          version: uiGlobals.appVersion
+        });
       });
     } else {
       console.log('Would have tracked Mixpanel event in a release build: ' + eventName);
