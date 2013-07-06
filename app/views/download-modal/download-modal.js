9d919d547d447e4f2f601564873ffd2ddd8713fb
Paul Baumgart <paul@proxv.com>
2013-07-05 19:09:22 -0700

    stylistic cleanups

--- app/views/download-modal/download-modal.js
+++ app/views/download-modal/download-modal.js
@@ -9,8 +9,8 @@ module.exports = BaseView.extend({
   className: 'download-modal',
 
   events: {
-      "click .button.cancel": "remove",
-      "click": "checkOutsideClick"
+    'click .button.cancel': 'remove',
+    'click': '_checkOutsideClick'
   },
 
   initialize: function() {
@@ -45,11 +45,11 @@ module.exports = BaseView.extend({
     });
   },
 
-  checkOutsideClick: function(evt){
-      var $target = $(evt.target);
-      if($target.parent('body').length){
-          this.remove();
-      }
+  _checkOutsideClick: function(evt){
+    var $target = $(evt.target);
+    if ($target.parent('body').length) {
+      this.remove();
+    }
   },
 
   show: function() {
@@ -58,7 +58,7 @@ module.exports = BaseView.extend({
 
   remove: function() {
     if (_.isFunction(this.options.onCancel)) {
-        this.options.onCancel();
+      this.options.onCancel();
     }
     this.$el.remove();
   }
