347bbfdac955a3e45dc0e9fba9996183abdd02d3
Paul Baumgart <paul@proxv.com>
2013-07-16 17:48:50 -0700

    reconnect to warehouse after new login

--- app/app-controller.js
+++ app/app-controller.js
@@ -312,8 +312,8 @@ AppController.prototype = {
   },
 
   _afterAuthorize: function() {
-      uiGlobals.isFirstRun = false;
-      this._markFirstRun();
+    uiGlobals.isFirstRun = false;
+    this._markFirstRun();
 
     this._paintMainApp();
 
@@ -338,7 +338,10 @@ AppController.prototype = {
     var authorizationView = new AuthorizationView();
     authorizationView.logOut(function() {
       authorizationView.remove();
-      this._authorize(this._paintMainApp.bind(this));
+      this._authorize(function() {
+        this._paintMainApp();
+        api.connectToStoreServer();
+      }.bind(this));
     }.bind(this));
   },
 
