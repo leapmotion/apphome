e8425697d3b6ec034e5048428e076be460e49783
Paul Mandel <paul.mand3l@gmail.com>
2013-09-09 15:38:35 -0700

    Factoring commonalities into tile.js module

--- app/views/trash-tile/trash-tile.js
+++ app/views/trash-tile/trash-tile.js
@@ -1,15 +1,13 @@
-var config = require('../../../config/config.js');
+var Tile = require('../tile/tile.js');
 
-var BaseView = require('../base-view.js');
-
-module.exports = BaseView.extend({
+module.exports = Tile.extend({
 
   viewDir: __dirname,
 
   initialize: function(args) {
-    this.injectCss();
     var leapApp = args.leapApp;
-    var modal = args.trash_modal;
+
+    this.initializeTile(leapApp);
 
     var templateData = leapApp.toJSON();
     templateData.iconPath = (templateData.iconPath ? this._makeFileUrl(templateData.iconPath) : '');
@@ -18,41 +16,7 @@ module.exports = BaseView.extend({
 
     this._showOrHideIcon();
 
-    this.listenTo(leapApp, 'change:tilePath', function() {
-      var tilePath = leapApp.get('tilePath');
-      if (tilePath) {
-        this.$('.tile-bg').attr('src', this._makeFileUrl(leapApp.get('tilePath'), true));
-      } else {
-        this.$('.tile-bg').attr('src', this._makeFileUrl(config.DefaultTilePath));
-      }
-      this._showOrHideIcon();
-    }, this);
-
-    this.listenTo(leapApp, 'change:iconPath', function() {
-      this.$('.icon').attr('src', this._makeFileUrl(leapApp.get('iconPath'), true));
-      this._showOrHideIcon();
-    }, this);
-
-    this.listenTo(leapApp, 'change:name', function() {
-      this.$('.name').text(leapApp.get('name'));
-    }, this);
-
     this.$el.click(this.options.onReinstall);
 
-    this.$el.attr('tile_id', leapApp.id);
-
-  },
-
-  _makeFileUrl: function(filePath, forceRefresh) {
-    return 'file://' + filePath + (forceRefresh ? '#' + (new Date()).getTime() : '');
-  },
-
-  _showOrHideIcon: function() {
-    if (this.options.leapApp.showIcon()) {
-      this.$('.icon').show();
-    } else {
-      this.$('.icon').hide();
-    }
   }
-
 });
