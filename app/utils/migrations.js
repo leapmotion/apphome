5a16be19f3283c17d0fd2c88aa66530d7f8c5cb6
Paul Mandel <paul.mand3l@gmail.com>
2013-09-18 20:56:06 -0700

    4 spaces -> 2 spaces

--- app/utils/migrations.js
+++ app/utils/migrations.js
@@ -17,48 +17,48 @@ need to be run in order.
 ***************/
 
 var Migrations = [
-    // Move single myApp Collection to myApp and uninstalledApps collections
-    function v0_to_v1() {
-        var uninstalledAppsJsonList = [];
-        var myAppsJsonList = [];
+  // Move single myApp Collection to myApp and uninstalledApps collections
+  function v0_to_v1() {
+    var uninstalledAppsJsonList = [];
+    var myAppsJsonList = [];
 
-        var appJsonList = db.fetchObj(config.DbKeys.InstalledApps);
+    var appJsonList = db.fetchObj(config.DbKeys.InstalledApps);
 
-        appJsonList.forEach(function sortApp(appJson) {
-            if (appJson.state === LeapApp.States.Uninstalled) {
-                uninstalledAppsJsonList.push(appJson);
-            } else {
-                myAppsJsonList.push(appJson);
-            }
-        });
+    appJsonList.forEach(function sortApp(appJson) {
+      if (appJson.state === LeapApp.States.Uninstalled) {
+        uninstalledAppsJsonList.push(appJson);
+      } else {
+        myAppsJsonList.push(appJson);
+      }
+    });
 
-        db.saveObj(config.DbKeys.InstalledApps, myAppsJsonList);
-        db.saveObj(config.DbKeys.UninstalledApps, uninstalledAppsJsonList);
-    }
+    db.saveObj(config.DbKeys.InstalledApps, myAppsJsonList);
+    db.saveObj(config.DbKeys.UninstalledApps, uninstalledAppsJsonList);
+  }
 ];
 
 // Migrate database to latest version
 // Should be idempotent
 function migrate() {
-    if (!db.fetchObj(config.DbKeys.DbVersion)) {
-        db.saveObj(config.DbKeys.DbVersion, 0);
-    }
+  if (!db.fetchObj(config.DbKeys.DbVersion)) {
+    db.saveObj(config.DbKeys.DbVersion, 0);
+  }
 
-    console.log('Checking if database needs to be migrated');
+  console.log('Checking if database needs to be migrated');
 
-    var currentDbVersion = db.fetchObj(config.DbKeys.DbVersion);
+  var currentDbVersion = db.fetchObj(config.DbKeys.DbVersion);
 
-    if (currentDbVersion < Migrations.length) {
-        console.log('Migrating database from version ' + currentDbVersion + ' to version ' + Migrations.length);
-        // Extract only the migrations that apply to the current db version.
-        Migrations.slice(currentDbVersion).forEach(function applyMigration(migration) {
-            migration();
-        });
+  if (currentDbVersion < Migrations.length) {
+    console.log('Migrating database from version ' + currentDbVersion + ' to version ' + Migrations.length);
+    // Extract only the migrations that apply to the current db version.
+    Migrations.slice(currentDbVersion).forEach(function applyMigration(migration) {
+      migration();
+    });
 
-        db.saveObj(config.DbKeys.DbVersion, Migrations.length);
-    } else {
-        console.log('Database already at version ' + currentDbVersion);
-    }
+    db.saveObj(config.DbKeys.DbVersion, Migrations.length);
+  } else {
+    console.log('Database already at version ' + currentDbVersion);
+  }
 }
 
 module.exports.migrate = migrate;
