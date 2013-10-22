2a81b686f8c6e45c478bb193e9de0b79e226b37e
Paul Mandel <paul.mand3l@gmail.com>
2013-10-22 12:03:56 -0700

    Adding more steps to the tutorial

--- app/utils/tutorial.js
+++ app/utils/tutorial.js
@@ -2,18 +2,40 @@ var i18n = require('./i18n.js');
 
 function makeGuides() {
     guiders.createGuider({
-        buttons: [{name: 'Take a Quick Tour', onclick: guiders.next}],
+        buttons: [{name: 'Take a Quick Tour', classString: 'primary', onclick: guiders.next}],
         description: 'Airspace Home is the place to launch all of your Leap-powered apps.',
-        id: 'start',
-        next: 'store',
+        id: 'g_start',
+        next: 'g_apps',
         title: 'Welcome to Airspace Home!',
         xButton: true,
     }).show();
 
     guiders.createGuider({
-        buttons: [{name: 'Next'}],
+        buttons: [{name: 'Next', classString: 'next'}],
+        description: "We thought you would like to dive right in, so we handpicked some free apps for you.",
+        id: 'g_apps',
+        next: 'g_orientation',
+        title: 'Experience the Controller',
+        attachTo: '#boom-ball',
+        position: 3,
+        highlight: '#boom-ball'
+    });
+
+    guiders.createGuider({
+        buttons: [{name: 'Back', classString: 'back'}, {name: 'Next', classString: 'next'}],
+        description: "Want to learn more about the Leap device's capabilities?  Go through a quick orientation to learn first hand.",
+        id: 'g_orientation',
+        next: 'g_store',
+        title: 'Understand the Device',
+        attachTo: '#orientation',
+        position: 6,
+        highlight: '#orientation'
+    });
+
+    guiders.createGuider({
+        buttons: [{name: 'Back', classString: 'back'}, {name: 'Done', classString: 'primary', onclick: guiders.hideAll}],
         description: "Airspace Store is the place for you to discover and download new apps.",
-        id: 'store',
+        id: 'g_store',
         title: 'Discover New Apps!',
         attachTo: '#airspace-store',
         position: 3,
