2428aff94e8bca9e5392d92476083b4e526378bc
Paul Mandel <paul.mand3l@gmail.com>
2013-11-05 19:14:54 -0800

    Setting up tutorial for internationalizing

--- app/utils/tutorial.js
+++ app/utils/tutorial.js
@@ -2,42 +2,42 @@ var i18n = require('./i18n.js');
 
 function makeGuides() {
     guiders.createGuider({
-        buttons: [{name: 'Take a Quick Tour', classString: 'primary', onclick: guiders.next}],
+        buttons: [{name: i18n.translate('Take a Quick Tour'), classString: 'primary', onclick: guiders.next}],
         classString: 'primary',
-        description: 'Airspace Home is the place to launch all of your Leap-powered apps.',
+        description: i18n.translate('Airspace Home is the place to launch all of your Leap-powered apps.'),
         id: 'g_start',
         next: 'g_apps',
-        title: 'Welcome to Airspace Home!',
+        title: i18n.translate('Welcome to Airspace Home!'),
         xButton: true,
     }).show();
 
     guiders.createGuider({
-        buttons: [{name: 'Next', classString: 'next'}],
-        description: "We thought you would like to dive right in, so we handpicked some free apps for you.",
+        buttons: [{name: i18n.translate('Next'), classString: 'next'}],
+        description: i18n.translate("We thought you would like to dive right in, so we handpicked some free apps for you."),
         id: 'g_apps',
         next: 'g_orientation',
-        title: 'Experience the Controller',
-        attachTo: '#boom-ball',
+        title: i18n.translate('Experience the Controller'),
+        attachTo: '.tile.store:nth-of-type(1)',
         position: 3,
-        highlight: '#boom-ball'
+        highlight: '.tile.store:nth-of-type(1)'
     });
 
     guiders.createGuider({
-        buttons: [{name: 'Back', classString: 'back'}, {name: 'Next', classString: 'next'}],
-        description: "Want to learn more about the Leap device's capabilities?  Go through a quick orientation to learn first hand.",
+        buttons: [{name: i18n.translate('Back'), classString: 'back'}, {name: i18n.translate('Next'), classString: 'next'}],
+        description: i18n.translate("Want to learn more about the Leap device's capabilities?  Go through a quick orientation to learn first hand."),
         id: 'g_orientation',
         next: 'g_store',
-        title: 'Understand the Device',
+        title: i18n.translate('Understand the Device'),
         attachTo: '#orientation',
         position: 6,
         highlight: '#orientation'
     });
 
     guiders.createGuider({
-        buttons: [{name: 'Back', classString: 'back'}, {name: 'Done', classString: 'primary', onclick: guiders.hideAll}],
-        description: "Airspace Store is the place for you to discover and download new apps.",
+        buttons: [{name: i18n.translate('Back'), classString: 'back'}, {name: i18n.translate('Done'), classString: 'primary', onclick: guiders.hideAll}],
+        description: i18n.translate("Airspace Store is the place for you to discover and download new apps."),
         id: 'g_store',
-        title: 'Discover New Apps!',
+        title: i18n.translate('Discover New Apps!'),
         attachTo: '#airspace-store',
         position: 3,
         highlight: '#airspace-store'
