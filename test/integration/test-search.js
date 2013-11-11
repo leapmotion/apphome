49d2ae6180ad65036abcbc39e0b384a83dbcd70a
Bj Gopinath <bgopinath@leapmotion.com>
2013-11-11 09:40:00 -0800

    Updating test-search with more case-sensitivity cases and switching to a new mocha reporter output

--- test/integration/test-search.js
+++ test/integration/test-search.js
@@ -5,7 +5,7 @@ describe('search', function() {
 
   var browser = wd.promiseChainRemote();
 
-  beforeEach(function(done) {
+  before(function(done) {
     this.timeout(40000);
     support.loadApp(browser)
       .then(function() {
@@ -17,17 +17,50 @@ describe('search', function() {
       });
   });
 
-  it('should filter to only show apps with case-insensitively matching names', function(done) {
-    browser
-      .elementByCssSelector('#search').type('orientation')
-      .elementsByCssSelector('.tile').then(function(elements) {
-        elements.length.should.equal(1);
-      }).then(function() {
+  describe('case-insensitivity', function() {
+    // Clear the search bar before each test
+    beforeEach(function(done) {
+      browser.elementByCssSelector('#search').clear()
+      .then(function() {
         done();
       });
-  });
+    });
 
-  afterEach(function(done) {
+    it('should accept all lower-case names', function(done) {
+      browser
+        .elementByCssSelector('#search').type('orientation')
+        .elementsByCssSelector('.tile').then(function(elements) {
+          elements.length.should.equal(1);
+          elements[0].text().should.eventually.equal('Orientation').and.notify(done);
+        });
+    });
+        
+    it('should accept a name with a leading upper-case character', function(done) {
+      browser
+        .elementByCssSelector('#search').type('Orientation')
+        .elementsByCssSelector('.tile').then(function(elements) {
+          elements.length.should.equal(1);
+        }).then(done, done);
+    });
+        
+    it('should accept all upper-case names', function(done) {
+      browser
+        .elementByCssSelector('#search').type('ORIENTATION')
+        .elementsByCssSelector('.tile').then(function(elements) {
+          elements.length.should.equal(1);
+        }).then(done, done);
+    });
+        
+    it('should accept mixed-case names', function(done) {
+      browser
+        .elementByCssSelector('#search').type('oRiEnTaTiOn')
+        .elementsByCssSelector('.tile').then(function(elements) {
+          elements.length.should.equal(1);
+        }).then(done, done);
+    });
+  });
+  
+  after(function(done) {
     this.timeout(10000);
     browser
       .quit()
