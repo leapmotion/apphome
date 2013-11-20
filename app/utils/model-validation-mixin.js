b405a9197c3f0eebfde314ef121254de50b6cc11
Paul Mandel <paul.mand3l@gmail.com>
2013-11-19 22:03:34 -0800

    First run at model validation

---
+++ app/utils/model-validation-mixin.js
@@ -0,0 +1,160 @@
+var _ = require ('underscore');
+var request = require('request');
+var url = require('url');
+var path = require('path');
+var fs = require('fs');
+
+module.exports = function(properties, config) {
+  if (config){
+      properties.validate = _validate(config);
+  } else if (properties.hasOwnProperty('validationConfig')) {
+    properties.validate = _validate(properties.validateionConfig);
+
+  }
+
+  return properties;
+};
+
+function _validate(config){
+  return function(attrs, options) {
+
+    _.each( config, function(config_item, key){
+
+        var config_data = _validate_config(config_item, key, attrs);
+
+      _.bind(_validate_item, this)(attrs[config_data.name], config_data);
+
+    }, this);
+ };
+}
+
+function _validate_config(config_item, key){
+  var e;
+  var config_data;
+  if (_.isString(config_item)){
+     config_data = {name: key, type: config_item};
+  } else {
+    if (!_.isObject(config_item)){
+       e = new Error('bad validation config: ');
+    }  else {
+      config_data = config_item;
+    }// end if  not name, type
+  } // end if string
+
+  // we should have set config_data to an object ; validate its name, type properties -- the miinimal config
+
+  if (!(object.name && object.type)){
+       e = new Error('validation definition needs name && type');
+    if (e){
+      e.config = config;
+      e.key = key;
+      e.config_item = config_item;
+      throw e;
+
+    }// end throw
+
+  return config_data;
+}
+
+function _validate_item(value, config_data){
+  if (_.isNull(value)){
+    if (!(config_data.hasOwnProperty('optional') && config_data.optional)){
+        return 'missing required field ' + config_data.name;
+    }
+  }
+
+  // at this point we have some sort of data for value.
+
+  switch ( config_data.type){
+
+    case 'url|path':
+    e = _validate_url_or_path(value, config_data.name, config_data.ext);
+    break;
+
+    case 'int':
+      e = _validate_int(value, config_data.name));
+    break;
+
+    case 'string':
+      e = _validate_string(value, config_data.name));
+      break;
+
+    default:
+    return 'bad config type ' + config_data.type;
+  }
+}
+
+var IMAGE_EXTENSIONS = ['jpg', 'png', 'gif', 'jpeg', 'bmp'];
+var ARCHIVE_EXTENSIONS = ['zip', 'gz', 'dmg', 'pkg', 'tar'];
+
+var IMAGE_OR_ARCHIVE_EXTENSIONS = IMAGE_EXTENSIONS.concat(ARCHIVE_EXTENSIONS);
+
+function _get_ext(ext){
+
+  if (!ext){
+    ext = IMAGE_OR_ARCHIVE_EXTENSIONS;
+  }
+
+    switch(ext){
+      case 'image':
+      ext = IMAGE_EXTENSIONS;
+      break;
+
+      case 'archive':
+      ext = ARCHIVE_EXTENSIONS;
+    }
+    return ext;
+}
+
+function _validate_url(value, name, ext){
+  var url_props = url.parse(value);
+
+
+  if (!url_props.pathname){
+    return 'no path for url ' + value + ' passed for ' + name;
+  } else {
+    ext = _get_ext(ext);
+    var extension = path.extname(url_props.pathname).toLowerCase();
+    if (!_.contains(ext, extension)){
+      return 'bad extension for ' + value + ' passed for ' + name;
+    }
+  }
+
+  return false;
+}
+
+function _is_url(value){
+    var data = url.parse(value);
+    return !!data.protocol;
+}
+
+function _validate_path(value, name){
+  if (!fs.existsSync(value)){
+    return 'file not found ' + value + 'passed for ' + name;
+  }
+  return false;
+}
+
+function _validate_url_or_path(value, name, ext){
+  if (_is_url(value)){
+    return _validate_url(value, name, ext);
+  } else {
+    return _validate_path(value, name);
+  }
+}
+
+function _validate_string(value, name){
+  if (!_.isString(value)){
+    return 'non string ' + value + ' passed for ' + name;
+  }
+  return false;
+}
+
+function  _validate_int(value, name){
+  if (! _.isNumber(value) ){
+    return 'non number ' + value + ' passed for ' + name;
+  } else if(Number(value) != Math.round(value)){
+    return 'non integer ' + value + ' passed for ' + name;
+  }
+  return false;
+}
