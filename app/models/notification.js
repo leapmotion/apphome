3bd3c6fc30bdcbf0335bf156aa8823346ed98a46
Paul Mandel <paul.mand3l@gmail.com>
2014-01-08 14:05:16 -0800

    Properly tracking viewed notifications

--- app/models/notification.js
+++ app/models/notification.js
@@ -6,7 +6,7 @@ var BaseModel = require('./base-model.js');
 var Notification = BaseModel.extend({
   initialize: function() {
     var viewedNotifications = db.fetchObj(config.DbKeys.ViewedNotifications) || [];
-    if (viewedNotifications.indexOf(this.get('id')) == -1) {
+    if (viewedNotifications.indexOf(this.get('uuid')) == -1) {
       this.set('new', true);
     }
   }
